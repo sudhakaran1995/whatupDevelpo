0b344cbf98057a4af31ff994fdd9aa1fa53bf977
sudhakaran1995 <71376083+sudhakaran1995@users.noreply.github.com>
2020-11-29 11:55:38 +0530

    initial files

---
+++ Test1.java
@@ -0,0 +1,17 @@
+package Project1;
+
+public class Test1 {
+public static void main(String[]args) {
+	String hunry ="yes";
+	if(hunry == "yes")
+		System.out.println("eat somting");
+	else
+	{
+	
+		System.out.println(" do work");
+		
+		
+	}
+}
+	
+}
